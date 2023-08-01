3541f5590c1bab2d6dc1cb3443286073d9028faf
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-08-01 16:47:08 -0300

    AP1 Iniciada

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
+
+enum TipoSanguineo {
+  aPositivo,
+  aNegativo,
+  bPositivo,
+  bNegativo,
+  oPositivo,
+  oNegativo,
+  abPositivo,
+  abNegativo,
+}
+
+class Pessoa {
+  const Pessoa({
+    required this.nome,
+    required this.email,
+    required this.telefone,
+    required this.github,
+    required this.tipoSanguineo,
+  });
+
+  final String nome;
+  final String email;
+  final String telefone;
+  final String github;
+  final TipoSanguineo tipoSanguineo;
+
+  // todo: implementar equals e hashcode
+}
+
+class EstadoListaDePessoas with ChangeNotifier {
+  final _listaDePessoas = <Pessoa>[];
+
+  List<Pessoa> get pessoas => List.unmodifiable(_listaDePessoas);
+
+  void incluir(Pessoa pessoa) {
+    _listaDePessoas.add(pessoa);
+    notifyListeners();
+  }
+
+  void excluir(Pessoa pessoa) {
+    _listaDePessoas.remove(pessoa);
+    notifyListeners();
+  }
+
+  // todo: implementar metodos restantes
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Hello, World!',
+      style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+}
